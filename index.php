eebf5976e75087bcf507e31791b9539507da45ab
cindyonggirawan <cindy.onggirawan@binus.ac.id>
2023-01-20 17:45:27 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, qui ea. Labore soluta, porro nam sequi officiis illum voluptas repellendus ab molestiae neque quis, hic in maiores vitae. Doloremque, expedita.</p>
+</body>
+</html>
